dd728d5c7a08f1d6767368b428c1381bfec6972a
10001shh <sghsri@gmail.com>
2018-08-14 00:44:01 -0500

    comment

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         $('#email').attr('href', 'mailto:sghsri@gmail.com');
     } else {
+        // for copying email on computer
         $("#email").on({
             mouseenter: function () {
                 $(this).html(
