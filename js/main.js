37e689823d94e206c48c7e8ab6213a96cc9119aa
Sriram Hariharan <sghsri@gmail.com>
2018-08-13 23:54:35 -0500

    more styling

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 $(function () {
-    $("#email").on({
-        mouseenter: function () {
-            $(this).html(
-                'sghsri@gmail.com (click to copy)'
-            );
-        },
-        mouseleave: function () {
-            $(this).html('<i class="fa fa-envelope" style="padding-right:5px;"><br></i> Contact');
-        },
-        click: function () {
-            copyToClipboard();
-            $(this).html(
-                'sghsri@gmail.com (copied!)');
-        }
-    });
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('#email').attr('href', 'mailto:sghsri@gmail.com');
+    } else {
+        $("#email").on({
+            mouseenter: function () {
+                $(this).html(
+                    'sghsri@gmail.com (click to copy)'
+                );
+            },
+            mouseleave: function () {
+                $(this).html('<i class="fa fa-envelope" style="padding-right:5px;"><br></i> Contact');
+            },
+            click: function () {
+                copyToClipboard();
+                $(this).html(
+                    'sghsri@gmail.com (copied!)');
+            }
+        });
+    }
 
     function copyToClipboard() {
         let input = document.createElement("input");
